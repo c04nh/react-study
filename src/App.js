cde63d52fc80cc50fce90a06abb24d25fa1bb461
Nahyun Cho <nhsally@naver.com>
2022-09-05 13:45:41 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+          <h1>WEB</h1>
+          world wide web!
+        </header>
+    );
+  }
+}
+
+class TOC extends Component{
+  render() {
+    return (
+      <nav>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    )
+  }
+}
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Hello, React!
+        <Subject/>
+        <TOC/>
+        <Content/>
       </div>
     );
   }
