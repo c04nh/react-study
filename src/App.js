19fc0771d0df6c40b05bba62cd571412934939c5
Nahyun Cho <nhsally@naver.com>
2022-09-05 15:01:39 +0900

    state, key

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"/>
-        <TOC/>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."/>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
+        <TOC data={this.state.contents}/>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."/> 
       </div>
     );
   }
