6230f1a2e4fd970263eee85d9cb87911b57cef7a
Nahyun Cho <nhsally@naver.com>
2022-09-06 15:56:11 +0900

    component 이벤트

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,11 +20,38 @@ class App extends Component {
     }
   }
   render(){
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      for(let i=0; i< this.state.contents.length; i++){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+      }
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
-        <TOC data={this.state.contents}/>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."/> 
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        />
+        <TOC 
+            onChangePage={function(id){
+              this.setState({
+                mode:'read',
+                selected_content_id:Number(id)});
+            }.bind(this)}
+            data ={this.state.contents}
+         />
+        <Content title={_title} desc={_desc}/> 
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -5,7 +5,17 @@ class TOC extends Component{
         var lists = [];
         var data = this.props.data;
         for(let i=0; i< data.length; i++){
-            lists.push(<li key={data[i].id}><a href={'/content/'+data[i].id}>{data[i].title}</a></li>)
+            lists.push(
+            <li key={data[i].id}>
+                <a 
+                    href={"/content/"+data[i].id}
+                    data-id={data[i].id}
+                    onClick={function(id, title, e){
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this, data[i].id, data[i].title)}
+                >{data[i].title}</a>
+            </li>)
           }
         return (
             <nav>
